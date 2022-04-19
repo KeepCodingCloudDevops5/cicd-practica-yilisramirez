3ed856ef51eb5c6051b73ebc003eef850bcdd25d
yilisnataly <39458920+yilisnataly@users.noreply.github.com>
2022-04-19 16:59:44 +0200

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,8 +1,14 @@
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.9.0"
+    }
+  }
   backend "s3" {
-    bucket = "var.s3_bucket_name[count.index]"
-    key = "terraform.tfstate"
-    region = "eu-west-1"
+    bucket  = "acme-storage-dev-kc-storage"
+    key     = "infra_dev/terraform.tfstate"
+    region  = "eu-west-1"
     encrypt = true
   }
 }
@@ -13,12 +19,10 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "storage" {
-  count = length(var.s3_bucket_name)
-  bucket = var.s3_bucket_name[count.index]
+  bucket = "${var.s3_bucket_name}-storage"
 
   tags = {
-    Name        = var.s3_bucket_name[count.index]
-    Environment = "ACME storage"
+    Name        = "${var.s3_bucket_name}-storage"
+    Environment = "ACME storage dev"
   }
 }
-
