230f821189bb05cfd1ff64526fd19e4589bb8fa8
yilisnataly <39458920+yilisnataly@users.noreply.github.com>
2022-04-30 12:25:33 +0200

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -14,7 +14,6 @@ terraform {
 }
 
 provider "aws" {
-  profile                  = var.aws_profile
   region                   = var.aws_region
   shared_config_files = [ "~/.aws/config" ]
   shared_credentials_files = ["~/.aws/credentials"]
