92a1d92e60a737e8db474886faab69e6b41aec13
yilisnataly <39458920+yilisnataly@users.noreply.github.com>
2022-04-30 20:31:48 +0200

    Delete CIDE/python-application-example/src directory

--- CIDE/python-application-example/src/application_example/example.py
+++
@@ -1,9 +0,0 @@
-import logging, coloredlogs
-logger=logging.getLogger(__name__)
-coloredlogs.install(level='DEBUG',logger=logger)
-logger.debug('Hola Keepcoders')
-
-def add_one(number):
-    return number+1
-
-print(add_one(2))
